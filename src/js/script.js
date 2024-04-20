12ad886ff6b93f4260838970ed61826ded6cf824
christ-saragih <bennefit.19@gmail.com>
2024-04-20 14:58:03 +0700

    CREATED: Button back to top

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,19 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const btnToTop = document.querySelector('#btnToTop');
 
-    console.log(window.pageYOffset);
-    console.log(fixedNav);
+    // console.log(window.pageYOffset);
+    // console.log(fixedNav);
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        btnToTop.classList.remove('hidden');
+        btnToTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        btnToTop.classList.remove('flex');
+        btnToTop.classList.add('hidden');
     }
 }
 
